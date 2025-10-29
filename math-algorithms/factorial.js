36ee5ee896bb91d7f27ca69858de472811ed3f05
Gideon Adeti <gideonadeti0@gmail.com>
2025-10-29 08:40:07 +0000

    feat: implement factorial algorithm with error handling

---
+++ math-algorithms/factorial.js
@@ -0,0 +1,22 @@
+/**
+ * Calculates the factorial of a number n (n!).
+ * @param {number} n - The number to compute the factorial for. Must be a non-negative integer.
+ * @returns {number} The factorial of n.
+ * @throws {Error} If input is a negative number.
+ * @description
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */
+const factorial = (n) => {
+  if (n < 0) {
+    throw new Error("Input must be a positive integer");
+  }
+
+  let result = 1;
+
+  for (let i = 2; i <= n; i++) {
+    result *= i;
+  }
+
+  return result;
+};
